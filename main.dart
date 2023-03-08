78fc1df090c993a154b4d33f67da4a3431ccd00f
Syifacipaaa <127282922+Syifacipaaa@users.noreply.github.com>
2023-03-08 13:01:22 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Selamat Pagi Dunia',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Selamat Pagi Dunia'),
+        ),
+        body: Center(
+          child: Text(
+            'Sudahkah kamu bersyukur hari ini?',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Random',
+          child: Icon(Icons.restart_alt),
+          onPressed: null,
+        ),
+      )
+    )
+  );
+}
